fb97015aebdeef376f2c9c8ef15d428d617f7423
Prince Gajera <prince_1616@live.co.uk>
2025-06-25 13:04:50 +0100

    test

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,8 +1,18 @@
 // app/api/products/route.ts
 import { getAllProducts } from "~/app/lib/getProducts";
 import { NextResponse } from "next/server";
+import { products } from "~/server/db/schema";
+import { db } from "~/server/db";
 
 export async function GET() {
-  const data = await getAllProducts();
-  return NextResponse.json(data);
+  const rows = await db.select().from(products);
+
+  const parsedRows = rows.map((row) => ({
+    ...row,
+    category: JSON.parse(row.category ?? "[]"),
+    downloads: JSON.parse(row.downloads ?? "[]"),
+    specifications: JSON.parse(row.specifications ?? "[]"),
+  }));
+
+  return NextResponse.json(parsedRows);
 }
