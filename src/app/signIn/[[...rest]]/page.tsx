63fd9bd2142f1ac6932f53b9bd277f9fc4157495
Prince Gajera <prince_1616@live.co.uk>
2025-07-02 16:31:37 +0100

    changed background image

--- src/app/signIn/[[...rest]]/page.tsx
+++ src/app/signIn/[[...rest]]/page.tsx
@@ -8,10 +8,10 @@ export default function SignInPage() {
       className="flex w-full items-center justify-center bg-cover bg-center"
       style={{
         backgroundImage:
-          "url('https://static.wixstatic.com/media/48bae8_59d5b67eb50648f599f92b1bd499f16c~mv2.png/v1/fill/w_1897,h_485,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/48bae8_59d5b67eb50648f599f92b1bd499f16c~mv2.png')",
+          "url('https://cdn-images.fleetnews.co.uk/thumbs/960x960/web-clean/1/root/rolec-ev-factory.png')",
       }}
     >
-      <div className="max-w-md justify-center px-8 py-8">
+      <div className="flex h-full w-full items-center justify-center rounded-xl bg-white/30 backdrop-blur-sm">
         <SignIn />
       </div>
     </div>
