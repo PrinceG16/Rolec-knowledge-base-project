e6ff3e254a5e3110a5ea8dbfe415e4782971a1e6
Prince Gajera <prince_1616@live.co.uk>
2025-07-04 10:24:47 +0100

    added console logs for vercel

--- src/app/_lib/logger.ts
+++ src/app/_lib/logger.ts
@@ -15,6 +15,8 @@ export const logger = winston.createLogger({
     //   (i.e., fatal, error, warn, and info, but not trace)
     //
     new winston.transports.File({ filename: "combined.log" }),
+
+    new winston.transports.Console(),
   ],
 });
 
@@ -22,10 +24,10 @@ export const logger = winston.createLogger({
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
-if (process.env.NODE_ENV !== "production") {
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.simple(),
-    }),
-  );
-}
+// if (process.env.NODE_ENV !== "production") {
+//   logger.add(
+//     new winston.transports.Console({
+//       format: winston.format.simple(),
+//     }),
+//   );
+// }
