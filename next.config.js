69ce142046eace06367c8d48200bf97262aed869
Prince Gajera <prince_1616@live.co.uk>
2025-07-07 09:02:29 +0100

    commiting because sentry

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
@@ -15,7 +11,6 @@ const config = {
     ],
     dangerouslyAllowSVG: true,
   },
-
   typescript: {
     ignoreBuildErrors: true,
   },
